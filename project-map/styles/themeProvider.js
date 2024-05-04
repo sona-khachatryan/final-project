08c2d5e7e930101ebfa163e6875459e47c5668dc
Sona Khachatryan <sona.khachatryannn@gmail.com>
2024-05-04 07:59:18 +0400

    preserving theme mode between reloads

--- project-map/styles/themeProvider.js
+++ project-map/styles/themeProvider.js
@@ -4,17 +4,25 @@ import {darkTheme, lightTheme} from '@/styles/theme';
 import {ThemeProvider} from '@mui/material';
 import {useDispatch, useSelector} from 'react-redux';
 
-import React, {useEffect, useState} from 'react';
-import {setMode, setTheme} from '@/styles/themeSlice';
+import React, {useEffect} from 'react';
+import {setMode} from '@/styles/themeSlice';
+
 
 function MyThemeProvider({children}) {
-   const mode = useSelector((state) => state.theme.mode);
-   const selectedTheme = mode === 'dark' ? darkTheme : lightTheme;
+   const dispatch = useDispatch();
+   const mode = useSelector(state => state.theme.mode);
+
+   useEffect(() => {
+      const themeModeFromLocalStorage = (localStorage.getItem('themeMode'));
+      if (themeModeFromLocalStorage) {
+         dispatch(setMode(themeModeFromLocalStorage));
+      }
+   }, [dispatch]);
+
    return (
-      <ThemeProvider theme={selectedTheme}>
+      <ThemeProvider theme={mode === 'dark' ? darkTheme : lightTheme}>
          {children}
       </ThemeProvider>
    );
 }
-
 export default MyThemeProvider;
